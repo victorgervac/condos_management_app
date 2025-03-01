0f69c05ab20ed435733c86d0b7d1163a85d8959b
victorgervac <victorgervacio33@gmail.com>
2025-02-28 23:44:54 -0700

    User initial profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def index; end
+
+  def show
+    @user = current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   root 'dashboards#index'
   resources :reservations
   resources :listings
+  resources :users
+
 end
