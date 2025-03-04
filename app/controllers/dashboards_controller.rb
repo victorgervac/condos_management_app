35ef627cedc1d14ca6caa6981170071f9e71f006
victorgervac <victorgervacio33@gmail.com>
2025-03-03 21:49:06 -0700

    pagy

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,10 @@
 class DashboardsController < ApplicationController
+  include Pagy::Backend
   def index
+    @per_page = params[:per_page].nil? ? 10 : params[:per_page]
+
     @todays_cleaning_tasks = CleaningTask.all
+    @pagy, @records = pagy(@todays_cleaning_tasks, items: @per_page)
     # where(cleaning_date: Date.today..(Date.tomorrow + 5.days))
   end
 end
--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,8 @@
 class ListingsController < ApplicationController
   include Pagy::Backend
   def index
+    @per_page = params[:per_page].nil? ? 10 : params[:per_page]
+
     @listings = Listing.all
     @pagy, @records = pagy(@listings, items: @per_page)
   end
