87c85a5192db47f4fce880c516d4ce28cce04534
victorgervac <victorgervacio33@gmail.com>
2025-03-24 22:19:01 -0600

    correct button

--- app/controllers/cleanings_controller.rb
+++ app/controllers/cleanings_controller.rb
@@ -1,12 +1,7 @@
 class CleaningsController < ApplicationController
   include Pagy::Backend
   def index
-    if params[:fetch_data]
-      AuthHostAway.run
-      render dashboards_path
-    else
-      @cleanings = Cleaning.all
-      @pagy, @records = pagy(@cleanings, items: @per_page)
-    end
+    @cleanings = Cleaning.all
+    @pagy, @records = pagy(@cleanings, items: @per_page)
   end
 end
