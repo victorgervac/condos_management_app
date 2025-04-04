6ea7c3dbff47831c1613ed227bc462861c8f77d4
victorgervac <victorgervacio33@gmail.com>
2025-04-03 22:43:45 -0600

    add user enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  enum role: { "admin": 0, "owner": 1, "guest": 2 }
 
   def name
     "#{self.first_name} #{self.last_name}"
