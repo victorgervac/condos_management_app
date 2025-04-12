f1d3644265c63e5ea583215b44982e25ba606be1
victorgervac <victorgervacio33@gmail.com>
2025-04-12 11:16:07 -0600

    fetc lisiting through ui

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
   include Pagy::Backend
   def index
+    AuthHostAway.run(data_type: params[:fetch_data]) if params[:fetch_data]
     @per_page = params[:per_page].nil? ? 10 : params[:per_page]
 
     @listings = Listing.all
